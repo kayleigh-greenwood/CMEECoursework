01f0d5f245e3c1a34968e0380aa577850eaa3c7d
Kayleigh Greenwood <kayleigh.greenwood21@imperial.ac.uk>
2021-10-22 08:08:14 +0100

    basic, functioning tuple practical

---
+++ week2/code/tuple.py
@@ -0,0 +1,33 @@
+birds = ( ('Passerculus sandwichensis','Savannah sparrow',18.7),
+          ('Delichon urbica','House martin',19),
+          ('Junco phaeonotus','Yellow-eyed junco',19.5),
+          ('Junco hyemalis','Dark-eyed junco',19.6),
+          ('Tachycineata bicolor','Tree swallow',20.2),
+        )
+
+# print these tuples on separate lines
+# as loop
+print("\n")
+for species in birds:
+    print("Latin Name:", species[0])
+    print("Common name:", species[1])
+    print("Mass:", species[2] ,"\n")
+
+
+
+
+
+# as list comprehension
+
+
+# Birds is a tuple of tuples of length three: latin name, common name, mass.
+# write a (short) script to print these on a separate line or output block by species 
+# 
+# A nice example output is:
+# 
+# Latin name: Passerculus sandwichensis
+# Common name: Savannah sparrow
+# Mass: 18.7
+# ... etc.
+
+# Hints: use the "print" command! You can use list comprehensions!
