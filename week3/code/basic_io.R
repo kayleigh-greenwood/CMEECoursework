49a037941b6dcb92cb5361d9f0cf14d9961c120a
Kayleigh Greenwood <kayleigh.greenwood21@imperial.ac.uk>
2021-11-06 20:48:54 +0000

    adjust comments on basic_io.R

--- week3/code/basic_io.R
+++ week3/code/basic_io.R
@@ -1,14 +1,22 @@
 # A simple script to illustrate R input output.
 # Run line by line and check inputs outputs to understand what is happening
 
-MyData <- read.csv("../data/trees.csv", header = TRUE) # import with headers
+MyData <- read.csv("../data/trees.csv", header = TRUE) 
+# import file inro variable, with headers
 
-write.csv(MyData, "../results/MyData.csv") # write it out as a new file
+write.csv(MyData, "../results/MyData.csv") 
+# write contents of MyData into MyData.csv file in results
 
-write.table(MyData[1,], file = "../results/MyData.csv", append=TRUE) # append to it
+write.table(MyData[1,], file = "../results/MyData.csv", append=TRUE) 
+# append=TRUE means append to the previously created .csv file
+# appends a table of MyData's headings and the first row
 
-write.csv(MyData, "../results/MyData.csv", row.names=TRUE) # write row names
+write.csv(MyData, "../results/MyData.csv", row.names=TRUE) 
+# write contents of MyData into MyData.csv file in results
+# write row names in the first column
 
-write.table(MyData, "../results/MyData.csv", col.names=FALSE) # ignore column names
+write.table(MyData, "../results/MyData.csv", col.names=FALSE) 
+# write contents of MyData into MyData.csv file in results
+# ignore column names, don't write them into csv
 
 print("Script complete")
