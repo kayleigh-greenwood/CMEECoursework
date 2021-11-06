3a8473873cf28a010850947c61840443d417e5b8
Kayleigh Greenwood <kayleigh.greenwood21@imperial.ac.uk>
2021-11-06 21:02:04 +0000

    improved comments on break.R

--- week3/code/break.R
+++ week3/code/break.R
@@ -1,10 +1,16 @@
-i <- 0 #Initialize i
+# Intro to breaking out of loops using 'break'
+
+i <- 0 
+# Set i as 0
     while(i < Inf) {
-        if (i == 10) {
+        # sets infinite while loop
+        if (i == 10) { 
             break 
-             } # Break out of the while loop! 
+            } 
+            # Break out of the while loop when i hits 10
         else { 
             cat("i equals " , i , " \n")
+            # print value of i
             i <- i + 1 # Add 1 until 10 is reached
     }
 }
