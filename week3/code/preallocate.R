3a3c047367f7e58b274acbb0907e263695f3a15d
Kayleigh Greenwood <kayleigh.greenwood21@imperial.ac.uk>
2021-11-06 22:29:03 +0000

    improved comments in preallocate.R

--- week3/code/preallocate.R
+++ week3/code/preallocate.R
@@ -1,48 +1,52 @@
-### for loop, which resizes a vector repeatedly
+#########################################################
+# Compares times of pre-allocation to no pre-allocation #
+#########################################################
 
+### for loop, which resizes a vector repeatedly ###
+
+# defines function NoPreallocFun
 NoPreallocFun <- function(x){
-    # creates a function which takes a number as input
+    # creates a function which takes one argument(x)
     a <- vector() 
     # empty vector
+
+
     for (i in 1:x) {
-        # for 1 to inputted number
+        # for each number in the inclusive sequence of 1 to inputted number 'x'
         a <- c(a, i)
-        # updates value of a to a new vector
-        # comprising of the old vector, with a new item
-        # that item being i, the number for this loop
-        # vector is resized in each iteration of the loop
-        # more memory has to be allocated each time
-        # print(a)
-        # print(object.size(a))
+        # uses concatenate to append i to vector 'a'
+        # vector is resized in each iteration of the loop, more memory has to be allocated each time
+
+        print(a)
+        print(object.size(a))
         # prints size of vector a
     }
 }
 
+# checks run time of NoPreallocFun
 system.time(NoPreallocFun(1000))
-# checks run time
 
-### pre-allocated vector
+
+### pre-allocated vector ###
 
 # if we pre-allocate a vector that fits all values,
 # R doesn't have to reallocate
 
+# defines function PreallocFun
 PreallocFun <- function(x){
-    # creates function that takes input of a vector
     a <- rep(NA, x) 
     # pre-allocated vector
-    # rep means replicate values in vector 'x'
-    # x input of rep is a vector
-    # eg rep(8,4) would replicate 8, 4 times
-    # so here, 'rep' replicates NA, x amount of times
+    # rep means replicate value 'NA' , 'x' amount of times
     # this creates a vector of the same size as the inputted number
     for (i in 1:x) {
-        # loops through numbers from 1 to x
+        # inclusive loops (including x)
         a[i] <- i
-        # updates current location in vector with number
-        # print(a)
-        # print(object.size(a))
-        # prints size of vector
+        # updates each location in a with it's index number
+        print(a)
+        print(object.size(a))
+        # prints size of vector 'a'
     }
 }
 
+# checks function PreallocFun
 system.time(PreallocFun(1000))
