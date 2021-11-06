7253a2ee230d260a086e21794537cca6cbb69dbb
Kayleigh Greenwood <kayleigh.greenwood21@imperial.ac.uk>
2021-11-06 21:08:29 +0000

    improved comments for next.R

--- week3/code/next.R
+++ week3/code/next.R
@@ -1,5 +1,9 @@
+# Intro to using 'next' to skip to next iteration of for loop. Only prints odd numbers
+
 for (i in 1:10) {
-  if ((i %% 2) == 0) # check if the number is even
+  # for each number in an inclusive sequence
+  if ((i %% 2) == 0) 
+  # check if the number is even
     next # pass to next iteration of loop. 
   print(i) # only prints odd numbers
 }
