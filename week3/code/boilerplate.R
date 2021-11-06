89103572b37af594a6e0f62e26bbd5d9c98177d3
Kayleigh Greenwood <kayleigh.greenwood21@imperial.ac.uk>
2021-11-06 21:16:30 +0000

    improved comments for boilerplate.R

--- week3/code/boilerplate.R
+++ week3/code/boilerplate.R
@@ -1,13 +1,21 @@
 # A boilerplate R script
 
 MyFunction <- function(Arg1, Arg2){
+  # creates function 'MyFunction' which takes two arguments, and begins to define it
+  # definition of function lies within {}
   
   # Statements involving Arg1, Arg2:
-  print(paste("Argument", as.character(Arg1), "is a", class(Arg1))) # print Arg1's type
-  print(paste("Argument", as.character(Arg2), "is a", class(Arg2))) # print Arg2's type
+  print(paste("Argument", as.character(Arg1), "is a", class(Arg1))) 
+  # print Arg1's type
+  print(paste("Argument", as.character(Arg2), "is a", class(Arg2))) 
+  # print Arg2's type
     
-  return (c(Arg1, Arg2)) #this is optional, but very useful
+  return (c(Arg1, Arg2)) 
+  # this is optional, but very useful
+  # ensures any changes made to the inputted arguments are applicable outside of the function(globally)
 }
 
-MyFunction(1,2) #test the function
-MyFunction("Riki","Tiki") #A different test
+MyFunction(1,2) 
+# test the function with numbers
+MyFunction("Riki","Tiki") 
+# A different test using strings
