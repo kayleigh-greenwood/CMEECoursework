b0a3b805664ee7c4ecf1aa69ec65536d0509a42c
Kayleigh Greenwood <kayleigh.greenwood21@imperial.ac.uk>
2021-11-07 23:31:35 +0000

    improved comments in GPDD.Data.R

--- week3/code/GPDD_Data.R
+++ week3/code/GPDD_Data.R
@@ -1,14 +1,19 @@
+#########################################
+## Create a world map which plots data ##
+#########################################
+
 # load maps package
 require(maps)
 require(ggplot2)
 require(mapdata)
+
 # load GPDD data
 load("../data/GPDDFiltered.RData")
 
 # create a world map (using the map function, read its help page, also google example using maps)
 # superimpose on the map all the locations from which we have data in the GPDD Dataframe
 
-mymap<- map() + points(gpdd$long, gpdd$lat, col="red", cex= 1.5, pch=20)
+mymap <- map() + points(gpdd$long, gpdd$lat, col="red", cex= 1.5, pch=20)
 
 pdf("../results/worldmap.pdf")
 print(mymap)
