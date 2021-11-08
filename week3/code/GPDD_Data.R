7889e4fc2cbfe370e3fee1bff9d6c8cb4b2d4565
Kayleigh Greenwood <kayleigh.greenwood21@imperial.ac.uk>
2021-11-08 08:15:07 +0000

    improved comments in GPDD_Data.R

--- week3/code/GPDD_Data.R
+++ week3/code/GPDD_Data.R
@@ -13,12 +13,10 @@ load("../data/GPDDFiltered.RData")
 # create a world map (using the map function, read its help page, also google example using maps)
 # superimpose on the map all the locations from which we have data in the GPDD Dataframe
 
-mymap <- map() + points(gpdd$long, gpdd$lat, col="red", cex= 1.5, pch=20)
-
 pdf("../results/worldmap.pdf")
-print(mymap)
+map()
+points(gpdd$long, gpdd$lat, col="red", cex= 1.5, pch=20)
 dev.off()
-
 
 # Question: looking at the map, what biases might you expect in any analysis based on the data represented?
 # Answer: 
