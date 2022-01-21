9e7bc2b6f634ef27a3d8f124469e7d224e583cb0
Kayleigh Greenwood <kayleigh.greenwood21@imperial.ac.uk>
2022-01-21 16:16:20 +0000

    Implimented Week1 feedback

--- week.1/code/CompileLaTeX.sh
+++ week.1/code/CompileLaTeX.sh
@@ -10,20 +10,31 @@
 input=$1 # set $1 to a variable so that it can be manipulated to remove the suffix
 
 if [[ $input == *.tex ]] # if there is a .tex suffic
-then # remove the suffix
+then 
+    # remove the suffix
     input=$(basename "$1" .tex)
     echo $input
+
+    # determine if there is a bibtex file
+    count=`ls -1 *.bib 2>/dev/null | wc -l` # sets the variable 'count' equal to the amount of .tif files in the current directory
+
+    ##Creates pdf
+    pdflatex $input.tex
+    if [ $count = 0 ]
+    then
+        # to avoid an error, only attempt this line if .bib file is present
+        bibtex $input
+    fi
+    pdflatex $input.tex
+    pdflatex $input.tex
+    evince $input.pdf &
+
+    ##Cleanup, removes intermediary files
+    rm *.aux
+    rm *.log
+    rm *.bbl
+    rm *.blg
+else
+    echo "Error: Please provide a LaTeX source code file (.tex) after the script name in the command line."
 fi
 
-##Creates pdf
-pdflatex $input.tex
-bibtex $input
-pdflatex $input.tex
-pdflatex $input.tex
-evince $input.pdf &
-
-##Cleanup, removes intermediary files
-rm *.aux
-rm *.log
-rm *.bbl
-rm *.blg
