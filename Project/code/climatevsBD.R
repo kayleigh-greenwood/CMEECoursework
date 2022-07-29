53362d72e94ac7ff5c491c41b701a28050043818
Kayleigh Greenwood <kayleigh.greenwood21@imperial.ac.uk>
2022-07-29 11:15:00 +0800

    writing up methods

--- Project/code/climatevsBD.R
+++ Project/code/climatevsBD.R
@@ -42,8 +42,9 @@ for (country in seq_along(matchingcountrys)){
   
   # fit model
   model <- (lm(BDvalues~Climatevalues))
-  
-  #check if p value is below 0.05 (make sure )
+  plot(Climatevalues, BDvalues)
+  abline(model)
+  #check if p value is below 0.05 (make sure)
   if (as.numeric(summary(model)$coefficients[,4][2]) < 0.05){
     
     #add correlation result to results data frame
@@ -70,6 +71,40 @@ resultsDF$continent <- countrycode(sourcevar = row.names(resultsDF),
 resultsDF$region <- countrycode(sourcevar = row.names(resultsDF),
                           origin = "country.name",
                           destination = "region")
+
+########### visualise results
+install.packages(c("cowplot", "googleway", "ggplot2", "ggrepel", "ggspatial", "libwgeom", "sf", "rnaturalearth", "rnaturalearthdata"))
+install.packages("classInt")
+install.packages('sf') #if this isn't working, try 'sudo apt install libudunits2-dev' in terminal
+library("ggplot2")
+theme_set(theme_bw())
+library("sf") 
+library("rnaturalearth")
+library("rnaturalearthdata")
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+class(world)
+
+ggplot(data = world) +
+  geom_sf() +
+  ggtitle("World map", subtitle = paste0("(", length(unique(world$NAME)), " countries)"))
+#########
+library('ggplot2')
+library('tidyverse')
+
+mapdata <- map_data("world")
+library(tibble)
+resultsDFmapping <- tibble::rownames_to_column(resultsDF, "country") # create new results DF where country is a column so that it can be joined with another df
+names(mapdata)[names(mapdata) == 'region'] <- 'country' # change the name of the countries column to match the other DF
+
+mapdata <- left_join(mapdata, resultsDFmapping, by='country') # join the data frames
+mapdata <- mapdata %>%  filter(!is.na(mapdata$corr))   # remove NAs
+
+map <- ggplot(mapdata, aes(x = long, y = lat, group = group)) +
+  geom_polygon(aes(fill= corr), colour = "black")
+
+map <- map + scale_fill_gradient(name = "Sensitivity Score", low = "red", high = "green", na.value = "grey50")
+map
 
 
 ####### try clustering by continent
